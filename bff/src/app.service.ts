3f494b70fdc446940dd1dccc4107b1cd4dbc319e
Vadzim Antonau <vanet@tut.by>
2022-08-15 16:57:28 +0300

    fix: Husky doesn't trigger

--- bff/src/app.service.ts
+++ bff/src/app.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  constructor(private helloText = 'Hello World!') {}
+
   getHello(): string {
-    return 'Hello World!';
+    return this.helloText;
   }
 }
