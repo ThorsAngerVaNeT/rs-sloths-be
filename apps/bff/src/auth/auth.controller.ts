734274b8cf7e43d5be2ee6f03cb076ba350d9663
Vadzim Antonau <vanet@tut.by>
2022-08-26 08:54:25 +0300

    fix(bff): replace github callback user instance check

--- apps/bff/src/auth/auth.controller.ts
+++ apps/bff/src/auth/auth.controller.ts
@@ -1,4 +1,3 @@
-/* eslint-disable class-methods-use-this */
 import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
@@ -10,6 +9,7 @@ import { GithubAuthGuard } from './guards/github.guard';
 export class AuthController {
   constructor(private jwtService: JwtService, private configService: ConfigService) {}
 
+  // eslint-disable-next-line class-methods-use-this
   @UseGuards(GithubAuthGuard)
   @Get('github')
   githubLogin() {
@@ -18,19 +18,17 @@ export class AuthController {
 
   @UseGuards(GithubAuthGuard)
   @Get('github/callback')
-  githubCallback(@Req() req: Request, @Res() res: Response) {
+  githubCallback(@Req() req: Request & { user: User }, @Res() res: Response) {
     const { user } = req;
-    if (user instanceof User) {
-      const { createdAt, ...payload } = user;
 
-      res.cookie('rs-sloths-cookie', this.jwtService.sign(payload), {
-        expires: new Date(Date.now() + +this.configService.get('JWT_EXPIRATION_TIME_MILLISECONDS')),
-        httpOnly: true,
-        secure: true,
-        domain: `${this.configService.get('FRONT_DOMAIN')}`,
-        sameSite: 'none',
-      });
-    }
+    const { createdAt, ...payload } = user;
+    res.cookie('rs-sloths-cookie', this.jwtService.sign(payload), {
+      expires: new Date(Date.now() + +this.configService.get('JWT_EXPIRATION_TIME_MILLISECONDS')),
+      httpOnly: true,
+      secure: true,
+      domain: `${this.configService.get('FRONT_DOMAIN')}`,
+      sameSite: 'none',
+    });
     res.redirect(`${this.configService.get('FRONT_URL')}`);
   }
 }
