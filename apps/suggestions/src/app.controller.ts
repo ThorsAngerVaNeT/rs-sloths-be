54eec3dba111c8e0c09f834f7b94c225d7576dbd
Vadzim Antonau <vanet@tut.by>
2022-08-28 21:41:38 +0300

    feat(suggestion): implement microservice controller and service

--- apps/suggestions/src/app.controller.ts
+++ apps/suggestions/src/app.controller.ts
@@ -1,12 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, UsePipes, ValidationPipe } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+import { Suggestion } from '@prisma/client';
+import { CreateSuggestionDto } from 'dto/create-suggestion.dto';
+import { UpdateSuggestionRatingDto } from 'dto/update-suggestion-rating.dto';
+import { GetAllConditions, ServiceResponse, SuggestionsAll } from './app.interfaces';
 import { AppService } from './app.service';
 
+@UsePipes(new ValidationPipe())
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern({ cmd: 'get_suggestions' })
+  async getSuggestions(params: GetAllConditions): Promise<ServiceResponse<SuggestionsAll>> {
+    return this.appService.getAll(params);
+  }
+
+  @MessagePattern({ cmd: 'get_suggestion' })
+  async getSuggestion(id: string): Promise<ServiceResponse<Suggestion>> {
+    return this.appService.getOne({ id });
+  }
+
+  @MessagePattern({ cmd: 'create_suggestion' })
+  async createSuggestion(createSuggestionDto: CreateSuggestionDto): Promise<ServiceResponse<Suggestion>> {
+    return this.appService.create(createSuggestionDto);
+  }
+
+  @MessagePattern({ cmd: 'delete_suggestion' })
+  async deleteSuggestion(id: string): Promise<ServiceResponse<Suggestion>> {
+    return this.appService.delete(id);
+  }
+
+  @MessagePattern({ cmd: 'update_rating' })
+  async updateSuggestionRating(
+    updateSuggestionRatingDto: UpdateSuggestionRatingDto
+  ): Promise<ServiceResponse<Pick<Suggestion, 'id' | 'rating'>>> {
+    return this.appService.updateRating(updateSuggestionRatingDto);
   }
 }
--- apps/suggestions/src/main.ts
+++ apps/suggestions/src/main.ts
@@ -3,15 +3,12 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.TCP,
-      options: {
-        port: +(process.env.PORT || 3004),
-      },
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      port: +(process.env.PORT || 3004),
     },
-  );
+  });
   await app.listen();
 }
 bootstrap();
