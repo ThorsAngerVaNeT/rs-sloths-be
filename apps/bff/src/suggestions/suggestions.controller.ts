ee5102c6a03d704d1e3775a8651ff40d59a1645c
Vadzim Antonau <vanet@tut.by>
2022-08-28 18:40:00 +0300

    feat(bff): add connection to microservice

--- apps/bff/src/suggestions/suggestions.controller.ts
+++ apps/bff/src/suggestions/suggestions.controller.ts
@@ -1,10 +1,16 @@
 /* eslint-disable class-methods-use-this */
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 import { CreateSuggestionDto } from './dto/create-suggestion.dto';
 import { UpdateSuggestionDto } from './dto/update-suggestion.dto';
 
 @Controller('suggestions')
 export class SuggestionsController {
+  constructor(
+    @Inject('SUGGESTIONS')
+    private readonly client: ClientProxy
+  ) {}
+
   @Post()
   create(@Body() createSuggestionDto: CreateSuggestionDto) {}
 
--- apps/bff/src/suggestions/suggestions.module.ts
+++ apps/bff/src/suggestions/suggestions.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { SuggestionsController } from './suggestions.controller';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'SUGGESTIONS',
+        transport: Transport.TCP,
+        options: {
+          port: 3004,
+        },
+      },
+    ]),
+  ],
   controllers: [SuggestionsController],
   providers: [],
 })
