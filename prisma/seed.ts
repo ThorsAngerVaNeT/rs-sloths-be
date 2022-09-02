68d69f175376c8458b6375ebdb1252ba2c60dd46
Vadzim Antonau <vanet@tut.by>
2022-09-02 08:55:27 +0300

    fix: sloths seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -28,7 +28,7 @@ async function main() {
       id: '579281ea-d2f4-43c7-ac51-5b304c705fc0',
       caption: 'Sloth Scientist',
       description: 'Clever looking sloth',
-      image_url: 'http://localhost:5173/memory-level-middle.png',
+      image_url: 'memory-level-middle.png',
     },
   });
   console.log('sloth1: ', sloth1);
@@ -38,7 +38,7 @@ async function main() {
       id: 'f2c34cee-4208-4b91-a404-c85f813c01b2',
       caption: 'Sloth Superman',
       description: 'Sloth Superman',
-      image_url: 'http://localhost:5173/memory-level-senior.png',
+      image_url: 'memory-level-senior.png',
     },
   });
   console.log('sloth2: ', sloth2);
