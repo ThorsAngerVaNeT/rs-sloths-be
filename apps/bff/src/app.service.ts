9a2f6c0955ac2674d6ad6003911d4cbe97ce1928
Vadzim Antonau <vanet@tut.by>
2022-09-04 11:31:15 +0300

    refactor(bff): update AppController and AppService, update .env.example

--- apps/bff/src/app.service.ts
+++ apps/bff/src/app.service.ts
@@ -1,9 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  // eslint-disable-next-line class-methods-use-this
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
+export class AppService {}
