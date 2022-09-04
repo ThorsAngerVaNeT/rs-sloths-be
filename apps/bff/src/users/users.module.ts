b9029da6ed9f3d92f91d048ff8eb458cb3f09988
Vadzim Antonau <vanet@tut.by>
2022-09-04 11:01:06 +0300

    refactor(bff): move logic from users controller to service

--- apps/bff/src/users/users.module.ts
+++ apps/bff/src/users/users.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { MICROSERVICES } from '../common/microservices.config';
 import { SlothsModule } from '../sloths/sloths.module';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 @Module({
   imports: [SlothsModule],
   controllers: [UsersController],
-  providers: [MICROSERVICES.USERS],
+  providers: [MICROSERVICES.USERS, UsersService],
 })
 export class UsersModule {}
